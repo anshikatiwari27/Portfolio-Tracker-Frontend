be752c62300f4de156d7625958e3a28e00bbfe09
Anshika Tiwari <112117600+anshikatiwari27@users.noreply.github.com>
2025-02-05 22:24:38 +0530

    Update Login.jsx

--- src/Components/LoginPage/Login.jsx
+++ src/Components/LoginPage/Login.jsx
@@ -2,18 +2,21 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import './LoginStyle.css';
-import backgroundImage from '../../assets/loginBG.jpg'; // Import your background image
+import backgroundImage from '../../assets/loginBG.jpg';
+
+
 
 function Login() {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const [error, setError] = useState('');
     const navigate = useNavigate();
-
+    
     const handleLogin = async (e) => {
         e.preventDefault();
-        try {
-            const response = await axios.post('http://localhost:8080/user/login', { userName: username, password: password });
+        try {   
+            console.log("LInk : " +`${import.meta.env.VITE_REACT_APP_API_URL}`);
+            const response = await axios.post(`${import.meta.env.VITE_REACT_APP_API_URL}/user/login`, { userName: username, password: password });
             if (response.status === 200) {
                 // Save the token in local storage
                 localStorage.setItem('token', response.data);
